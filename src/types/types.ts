c38ab146550eb07ef7f6bd6916429eceee918296
Alfredo Moreno <alu0101515601@ull.edu.es>
2025-03-27 13:51:53 +0000

    Creación de RequestType

---
+++ src/types/types.ts
@@ -0,0 +1,16 @@
+import { Funko } from "./models/Funko.js";
+
+/**
+ * Tipo de operación que puede realizar el cliente.
+ */
+export type OperationType = 'add' | 'update' | 'remove' | 'read' | 'list';
+
+/**
+ * Estrcuturta de la petición que envía el cliente al servidor.
+ */
+export interface RequestType {
+    type: OperationType;
+    user: string;
+    id?: number;
+    funko?: Funko;
+}
