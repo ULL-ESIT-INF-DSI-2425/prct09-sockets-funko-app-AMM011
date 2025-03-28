59331673367fd09f0790183995aaf814321e1fe7
Alfredo Moreno <alu0101515601@ull.edu.es>
2025-03-28 19:41:56 +0000

    Finalización de funko-cliente.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,24 +3,24 @@ import { Funko } from "../models/Funko.js";
 /**
  * Tipo de operación que puede realizar el cliente.
  */
-export type OperationType = 'add' | 'update' | 'remove' | 'read' | 'list';
+export type OperationType = "add" | "update" | "remove" | "read" | "list";
 
 /**
  * Estrcuturta de la petición que envía el cliente al servidor.
  */
 export interface RequestType {
-    type: OperationType;
-    user: string;
-    id?: number;
-    funko?: Funko;
+  type: OperationType;
+  user: string;
+  id?: number;
+  funko?: Funko;
 }
 
 /**
  * Estrucutra de la respuesta que envía el servidor al cliente.
  */
 export interface ResponseType {
-    type: OperationType;
-    success: boolean;
-    message: string;
-    funkoPops?: Funko[]; // usado para 'list' o 'read'
+  type: OperationType;
+  success: boolean;
+  message: string;
+  funkoPops?: Funko[]; // usado para 'list' o 'read'
 }
